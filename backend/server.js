5dddd81675b785d6d8dba4750abdda56b0c87b76
Ayush <ayushbhardwaj3746@gmail.com>
2025-08-01 15:58:57 +0530

    fix the script

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const express= require("express");
 const cors= require("cors");
 
 const path= require("path");
-const connectDB = require("./config/db");
+const connectDB = require("./config/db");   
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
@@ -35,4 +35,4 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
-app. listen(PORT, () => console. log( `Server running on port ${PORT}`)); 
+app. listen(PORT, () => console.log( `Server running on port ${PORT}`)); 
